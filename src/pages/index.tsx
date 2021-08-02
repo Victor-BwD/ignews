7e08ec268a58d334b69bfc10272ba37140552d19
Victor-BwD <victor.bogdanowdornelles@gmail.com>
2021-08-02 02:05:59 -0300

    [UPDATED] SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { stripe } from '../services/stripe'
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -34,7 +38,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JJsbfLUizU9fjslTRS7l9BI')
 
   const product = {
@@ -49,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
